2daea6fe5ea9fe36a766311c8cb1213d84b9fecd
athanasios.liapis@oxfordcc.co.uk <athanasios.liapis@oxfordcc.co.uk>
2021-11-28 18:45:59 +0000

    Created the project on VB using .Net Framework and MVC

---
+++ HumanResourcesApp/HumanResourcesApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HumanResourcesApp.Startup))]
+namespace HumanResourcesApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
